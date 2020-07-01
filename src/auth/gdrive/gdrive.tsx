0f76b98448022e841045ae8319cddf60e03bef34
slimfire <personalprojectyohannes@gmail.com>
2020-06-30 21:44:13 -0400

    [TASK] Add OneDrive auth button

--- src/auth/gdrive/gdrive.tsx
+++ src/auth/gdrive/gdrive.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import OneDriveLogin from 'react-microsoft-login';
 
 class GDriveAuth extends React.Component {
     public onResponse(res: any, type: string) {
@@ -7,15 +8,28 @@ class GDriveAuth extends React.Component {
         console.log(res);
     }
 
+    public oneDriveOnResponse(err: any, data: any) {
+        console.log({
+            err, data
+        });
+    }
+
     public render() {
         return (
-            <GoogleLogin
-                clientId="609636173272-ibmdbh1uki3smkdqjbrga0ig3490mhc8.apps.googleusercontent.com"
-                onSuccess={(res) => { this.onResponse(res, 'success');}}
-                onFailure={(res) => { this.onResponse(res, 'error')}}
-                buttonText="Google Drive"
-                isSignedIn={true}
-            />
+            <React.Fragment>
+                <GoogleLogin
+                    clientId="609636173272-ibmdbh1uki3smkdqjbrga0ig3490mhc8.apps.googleusercontent.com"
+                    onSuccess={(res) => { this.onResponse(res, 'success');}}
+                    onFailure={(res) => { this.onResponse(res, 'error')}}
+                    buttonText="Google Drive"
+                    isSignedIn={true}
+                />
+                <br/>
+                <OneDriveLogin 
+                    clientId="12f99e3f-599e-47ce-b76f-966d36fbe5d5"
+                    authCallback={this.oneDriveOnResponse}
+                />
+            </React.Fragment>
         );
     }
 }
