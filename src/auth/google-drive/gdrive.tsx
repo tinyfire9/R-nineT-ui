4d645d21c33260465f283861c5164c2b1ff486af
slimfire <personalprojectyohannes@gmail.com>
2020-07-01 21:02:57 -0400

    [TASK] add gapi.js

---
+++ src/auth/google-drive/gdrive.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+class GDriveAuth extends React.Component {
+    public componentDidMount(){
+        let script = document.createElement("script");
+        script.src = "https://apis.google.com/js/api.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }
+
+    public onResponse(res: any, type: string) {
+        console.log(type);
+        console.log(res);
+    }
+
+    public render() {
+        return (
+            <div>
+                <GoogleLogin
+                    clientId="609636173272-ibmdbh1uki3smkdqjbrga0ig3490mhc8.apps.googleusercontent.com"
+                    onSuccess={(res) => { this.onResponse(res, 'success');}}
+                    onFailure={(res) => { this.onResponse(res, 'error')}}
+                    buttonText="Google Drive"
+                    isSignedIn={true}
+                    
+                />
+                <br/>
+            </div>
+        );
+    }
+}
+
+export default GDriveAuth;
