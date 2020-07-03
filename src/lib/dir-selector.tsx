fdb97b6d7d16365b9d194ccc47c3d25c79d2eaf8
slimfire <personalprojectyohannes@gmail.com>
2020-07-02 22:06:39 -0400

    [TASK] add inputbox & transfer buttton

--- src/lib/dir-selector.tsx
+++ src/lib/dir-selector.tsx
@@ -4,6 +4,7 @@ import { SubDirectory } from '../interfaces';
 interface DirSelectorViewProps {
     subdirectory: SubDirectory[];
     listSubDir(path: string, name: string): any;
+    transferDirectories(dirs: string[]): any;
 }
 
 interface DirSelectorViewState {
@@ -13,7 +14,8 @@ interface DirSelectorViewState {
 class DirSelectorView extends React.Component<DirSelectorViewProps, DirSelectorViewState>{
     public static defaultProps = {
         subdirectory: [],
-        listSubDir: () => {}
+        listSubDir: () => {},
+        transferDirectories: () => {}
     };
 
     constructor(props: DirSelectorViewProps) {
@@ -27,21 +29,36 @@ class DirSelectorView extends React.Component<DirSelectorViewProps, DirSelectorV
     public onSelectedSubDirListUpdate(path: string) {
         const { selected_subdirectories } = this.state;
 
-        if(!selected_subdirectories[path]) {
-            let newState: DirSelectorViewState = { ...this.state };
-            newState.selected_subdirectories[path] = true;
-            this.setState(newState);
-        }
+        let newState: DirSelectorViewState = { ...this.state };
+        newState.selected_subdirectories[path] = !selected_subdirectories[path] ? true : false;
+        this.setState(newState);
     }
 
-    public makeTableRow() {
-        return [(<tr><td>..</td></tr>), ...this.props.subdirectory
+    private getSelectedDirectories(){
+        let { selected_subdirectories } = this.state;
+
+        return Object.keys(selected_subdirectories)
+            .filter((dir: string) => {
+                return selected_subdirectories[dir] == true;
+            });
+    }
+
+    public makeTableRows() {
+        let { selected_subdirectories } = this.state;
+
+        return this.props.subdirectory
             .map(({ name, path, type }: SubDirectory) => {
                 return (
                     <tr>
                         <td>
-                            <div 
+                            <input 
+                                type="checkbox"
+                                checked={selected_subdirectories[`${path}/${name}`]}
                                 onClick={() => this.onSelectedSubDirListUpdate(`${path}/${name}`)}
+                            />
+                        </td>
+                        <td>
+                            <div
                                 onDoubleClick={() => {
                                     if(type == 'Directory') {
                                         this.props.listSubDir(path, name);
@@ -56,25 +73,31 @@ class DirSelectorView extends React.Component<DirSelectorViewProps, DirSelectorV
                         <td>-</td>
                     </tr>
                 );
-            })];
+            });
     }
 
     public render() {
-        return (
-            <table className="drive-dir-selector-table" style={{border: 'black'}}>
-                <thead>
-                    <tr>
-                        <td>File Name</td>
-                        <td>File Type</td>
-                        <td>File Size</td>
-                        <td>Last Modified</td>
-                    </tr>
-                </thead>
-                <tbody>
+        let selected_subdirectories = this.getSelectedDirectories();
 
-                    { this.makeTableRow() }
-                </tbody>
-            </table>
+        return (
+            <React.Fragment>
+                <table className="drive-dir-selector-table" style={{border: 'black'}}>
+                    <thead>
+                        <tr>
+                            <td>File Name</td>
+                            <td>File Type</td>
+                            <td>File Size</td>
+                            <td>Last Modified</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.makeTableRows() }
+                    </tbody>
+                </table>
+                <button onClick={() => this.props.transferDirectories(selected_subdirectories)}>
+                    Transfer {selected_subdirectories.length} items >  
+                </button>
+            </React.Fragment>
         );
     }
 }
