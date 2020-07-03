dab8f8829444c4984ff2d15fa4d799d771c80e87
slimfire <personalprojectyohannes@gmail.com>
2020-07-03 19:31:25 -0400

    [TASK] render gdrive view

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import GDriveApp from '../src/app/gdrive';
 import GDriveAuth from './auth/gdrive/gdrive';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ textAlign: "center" }}>
-          <h1>Drive App</h1>
-          <GDriveAuth />
-        </div>
-      </header>
-    </div>
-  );
+class GDrive extends React.Component<any, any> {
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div style={{ textAlign: "center" }}>
+            <h1>Drive App</h1>
+            <GDriveAuth />
+            <GDriveApp />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GDrive;
+
