29b2db480028a7e8f0cddc251a82ea0b6b7d5832
slimfire <personalprojectyohannes@gmail.com>
2020-07-09 11:20:08 -0400

    [TASK] add dir type enum

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,11 @@
-
-export type Drive = 'GoogleDrive' | 'OneDrive' | 'BoxDrive' | 'DropboxDrive' | 'AmazonDrive';
-
-export type DirectoryType = 'File' | 'Directory';
+export enum DIRECTORY_TYPE {
+     'FILE' = 'file',
+     'DIRECTORY' = 'directory'
+}
 
 export interface DirectoryBase {
     name: string;
-    type: DirectoryType;
+    type: DIRECTORY_TYPE;
 }
 
 export interface GDriveDirectory extends DirectoryBase {
@@ -13,31 +13,3 @@ export interface GDriveDirectory extends DirectoryBase {
 }
 
 export type DriveDirectory = DirectoryBase | GDriveDirectory;
-
-export interface DriveState {
-    drive_name: Drive;
-    active: boolean;
-    window: 'left' | 'right' | 'none';
-    session: {
-        token: string;
-        active: boolean;
-        name: string;
-        image_url: string;
-        email: string;
-        expiration_time: number;
-    },
-    active_path: string;
-    selected_items: string[];
-}
-
-export interface TransferSession {
-    name: string;
-    source_drive: Drive;
-    destination_drive: Drive;
-    transfer_percentage: number;
-}
-
-export interface StoreState {
-    drives: DriveState[];
-    transfer_sessions: TransferSession[];
-}
