aef9e66674473334105323254c971de7ddc15f03
slimfire <personalprojectyohannes@gmail.com>
2020-09-23 15:50:16 -0400

    RNINET-45 [FEATURE] Add box auth component

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,5 @@
 import GDriveAuth from './gdrive/gdrive';
 import OneDriveAuth from './one-drive/one-drive';
+import BoxAuth from './box/box';
 
-export { GDriveAuth, OneDriveAuth };
+export { GDriveAuth, OneDriveAuth, BoxAuth };
