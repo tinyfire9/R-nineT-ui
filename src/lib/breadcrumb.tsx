c0e00b9892fdb6cd07f858607bdf3e07043d05ad
slimfire <personalprojectyohannes@gmail.com>
2020-10-12 16:35:39 -0400

    RNINET-11 [FEATURE] replace Breadcrumbs component

--- src/lib/breadcrumb.tsx
+++ src/lib/breadcrumb.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DIRECTORY_TYPE } from '../constants';
+import { IBreadcrumbProps, Breadcrumbs } from '@blueprintjs/core';
 
 export interface BreadCrumbItem {
     id: string;
@@ -17,31 +18,21 @@ class Breadcrumb extends React.Component<BreadCrumbProps, any> {
         breadcrumbItems: [],
         fetchSubDirectories() {}
     }
+
+    private renderBreadcrumb  =({ text, ...props }: IBreadcrumbProps)  => (
+        <Breadcrumb>{text}</Breadcrumb>
+    );
+
     public render() {
-        let breadcrumbItems = this.props.breadcrumbItems.map(
-            ({ name, id, type }: BreadCrumbItem, i: number) => {
-                return (
-                    <span key={id}>
-                        <a
-                            style={
-                                {
-                                    cursor: 'pointer',
-                                    color: (type == DIRECTORY_TYPE.DIRECTORY) ? 'blue': '',
-                                    borderBottom: (type == DIRECTORY_TYPE.DIRECTORY) ? '1px solid blue': ''
-                                }
-                            }
-                            onClick={() => this.props.fetchSubDirectories(id)} >{name}
-                        </a>
-                        <span>/</span> 
-                    </span>
-                );
-            }
-        );
-
-        breadcrumbItems.pop();
-
         return (
-            <div>{ breadcrumbItems }</div>
+            <Breadcrumbs 
+                currentBreadcrumbRenderer={this.renderBreadcrumb}
+                items={this.props.breadcrumbItems.map(({ name, id, type}: BreadCrumbItem) => ({
+                        text: name,
+                        onClick: () => this.props.fetchSubDirectories(id),
+                    })
+                )}
+            />
         );
     }
 }
